d0e20f3793e3a2439b527a6eb426b06befc372e9
CnHutNick <1130918652@qq.com>
2019-03-03 08:54:16 +0800

    Initial commit

---
+++ src/com/nick/test/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.nick.test;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class HelloWorld {
+    //psvm
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("HelloWorld.main");
+        System.out.println("args = [" + args + "]");
+        ArrayList list=new ArrayList();
+        for (Object o : list) {
+            
+        }
+    }
+
+
+}
---
+++ vhjvhj/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Nick
+  Date: 2019-03-01
+  Time: 13:03
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>信息不对u</title>
+  </head>
+  <body>
+<h1>蝴蝶蝴蝶</h1>
+  </body>
+</html>
